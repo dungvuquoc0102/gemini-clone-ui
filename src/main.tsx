56ae1a8610c8c136d60b0af7a74c5631acccc69c
Vu Quoc Dung <vuquocdung@192.168.2.201>
2025-05-27 17:57:14 +0700

    init

---
+++ src/main.tsx
@@ -0,0 +1,14 @@
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import AppRoutes from "./routes/AppRoutes";
+import { BrowserRouter } from "react-router-dom";
+import { store } from "./store/store";
+import { Provider } from "react-redux";
+
+createRoot(document.getElementById("root")!).render(
+  <Provider store={store}>
+    <BrowserRouter>
+      <AppRoutes />
+    </BrowserRouter>
+  </Provider>
+);
